498fd690912113840c0c31c252b6de42d9cf5ee4
Lizhonngxi <15961170229@163.com>
2018-01-04 16:16:17 +0800

    Project

---
+++ 03.Src/Calculator.Core/Calculator.Core/Opt/Percent.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator.Core.Opt
+{
+    class Percent: Operation
+    {
+        public override object GetResult()
+        {
+            double result = 0;
+            result = NumberA +((NumberA* NumberB)/100);
+            return result;
+        }
+    }
+}
