4eeda1a690ced7b615ed10e3d84918303f85dbce
mmmhong <mh_jsnt@163.com>
2018-01-05 16:24:58 +0800

    maohong-view

--- 03.Src/Calculator/Calculator.MH.View/Program.cs
+++ 03.Src/Calculator/Calculator.MH.View/Program.cs
@@ -16,7 +16,7 @@ namespace Calculator.MH.View
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frm_Calculator());
         }
     }
 }
