292ba6f7658d6370fcae65a6a74a76dfa7dc5372
wangrui9607 <wangrui9607@163.com>
2018-01-06 14:19:02 +0800

    新增Memory类

---
+++ 03.Src/Calculator/Calculator.Core/Save/Memory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Core.Save
+{
+    class Memory
+    {
+        List<double> MemoryNumber = new List<double>();
+
+        public void MemorySave(double num)
+        {
+            if (num != 0)
+            {
+                MemoryNumber.Add(num);
+            }
+        }
+
+        public double MemoryRead()
+        {
+            return MemoryNumber[1];
+        }
+
+        public void MemoryClean()
+        {
+
+        }
+    }
+}
