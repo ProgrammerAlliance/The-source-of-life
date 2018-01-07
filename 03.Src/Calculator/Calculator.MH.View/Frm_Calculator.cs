814d5eda0dc38d39b88c4070696e44aaaec2a2d5
mmmhong <mh_jsnt@163.com>
2018-01-07 15:41:08 +0800

    1

--- 03.Src/Calculator/Calculator.MH.View/Frm_Calculator.cs
+++ 03.Src/Calculator/Calculator.MH.View/Frm_Calculator.cs
@@ -1,23 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿
+using System;
 using System.Windows.Forms;
-using Calculator.Core;
 
 namespace Calculator
 {
 
     public partial class Frm_Calculator : Form
     {
-        ScreenData screenData = new ScreenData();
-        IOperation oper;
+        //ScreenData screenData = new ScreenData();
+
+        //Operation oper = new Operation();
+        //Special_Operation speOper = new Special_Operation();
+
+
+
+
+
+
+        private string _symbol = null;
+        private bool _lastIsSymbol = false;
+        private bool _lastIsSpecial = false;
+        private string result = "0";
+
 
         public Frm_Calculator()
         {
@@ -28,7 +32,7 @@ namespace Calculator
         {
             this.MaximizeBox = false;
             labOfLine.SendToBack();
-            ScreenDisplay();
+            //ScreenDisplay();
         }
 
         /// <summary>
@@ -38,9 +42,10 @@ namespace Calculator
         /// <param name="e"></param>
         private void btn_num_Click(object sender, EventArgs e)
         {
-            string strBtn = ((Button)sender).Text;
-            screenData.ProcessNum(strBtn);
-            ScreenDisplay();
+            //string strBtn = ((Button)sender).Text;
+            //screenData.ProcessNum(strBtn, _lastIsSymbol);
+            //ScreenDisplay();
+            //_lastIsSymbol = false;
         }
 
 
@@ -54,23 +59,56 @@ namespace Calculator
         /// <param name="e"></param>
         private void btn_Syb_Click(object sender, EventArgs e)
         {
-            string strBtn = ((Button)sender).Text;
+            //string strBtn = ((Button)sender).Text;
 
-            oper = OperationFactory.CreatOperation(strBtn);
-            oper.NumberB = Convert.ToDouble(screenData.Lab_Answer);
-            screenData.ProcessSymbol(strBtn);
+            ////获取加减乘除方法，为null则为第一次计算
+            ////_symbol初始为null，为第一次计算
+            //oper = OperationFactory.creatOperation(_symbol);
 
-            //计算
-            
+            //if (oper == null)//oper为空为第一次计算
+            //{
+            //    result = screenData.Lab_Answer;
+            //    //oper.NumberB = Convert.ToDouble(result);
+            //}
+            //else
+            //{
+            //    oper.NumberA = Convert.ToDouble(result);
+            //    oper.NumberB = Convert.ToDouble(screenData.Lab_Answer);
+            //    result = oper.GetResult().ToString();
+            //}
+            //_symbol = strBtn;//该符号用作下次计算
+
+            //screenData.ProcessSymbol(strBtn);
+
+            //screenData.Lab_Answer = result;
+            //ScreenDisplay();
+            //_lastIsSymbol = true;
+        }
 
 
+        /// <summary>
+        /// 根号，百分号，倒数特殊算法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Special_Click(object sender, EventArgs e)
+        {
+            //    string strBtn = ((Button)sender).Text;
+            //    if (!_lastIsSpecial)
+            //    {
+            //        speOper = new Special_Operation();
+            //        speOper = SpecalFactory.creatSpecial_Operation(strBtn);
+            //        speOper.NumberA = Convert.ToDouble(screenData.Lab_Answer);
+            //        speOper.StrFormula = speOper.NumberA.ToString();
+            //    }
 
-            string result = oper.GetResult().ToString();
+            //    speOper.GetResult();
 
-            //计算
+            //    screenData.Lab_Answer = speOper.NumberA.ToString();
 
-            screenData.Lab_Answer = result;
-            ScreenDisplay();
+            //    ScreenDisplay();
+            //    _lastIsSpecial = true;
+
         }
 
 
@@ -82,8 +120,8 @@ namespace Calculator
         /// <param name="e"></param>
         private void btn_clear_Click(object sender, EventArgs e)
         {
-            screenData = new ScreenData();
-            ScreenDisplay();
+            //screenData = new ScreenData();
+            //ScreenDisplay();
         }
         /// <summary>
         /// 清空输入框
@@ -92,8 +130,8 @@ namespace Calculator
         /// <param name="e"></param>
         private void btn_CE_Click(object sender, EventArgs e)
         {
-            screenData.Lab_Answer = "0";
-            ScreenDisplay();
+            //screenData.Lab_Answer = "0";
+            //ScreenDisplay();
         }
 
         /// <summary>
@@ -103,14 +141,14 @@ namespace Calculator
         /// <param name="e"></param>
         private void btn_del_Click(object sender, EventArgs e)
         {
-            screenData.DelNumber();
-            ScreenDisplay();
+            //screenData.DelNumber();
+            //ScreenDisplay();
         }
 
         private void btn_negate_Click(object sender, EventArgs e)
         {
-            screenData.Lab_Answer = (-(Convert.ToDouble(lab_answer.Text))).ToString();
-            ScreenDisplay();
+            //    screenData.Lab_Answer = (-(Convert.ToDouble(lab_answer.Text))).ToString();
+            //    ScreenDisplay();
         }
 
 
@@ -126,18 +164,15 @@ namespace Calculator
         #region 自定义方法
 
 
-        public void Count()
-        {
-
-        }
 
 
-        public void ScreenDisplay()
-        {
-            lab_formula.Text = screenData.Lab_Formula;
-            lab_answer.Text = screenData.Lab_Answer;
-            lab_register.Text = screenData.Lab_Register;
-        }
+        //public void ScreenDisplay()
+        //{
+        //    lab_formula.Text = screenData.Lab_Formula;
+        //    lab_answer.Text = screenData.Lab_Answer;
+        //    lab_register.Text = screenData.Lab_Register;
+        //}
+
 
 
         #endregion
