755a3d59a2e31e3cccf42f75863f1328dda47fc5
mmmhong <mh_jsnt@163.com>
2018-01-07 12:33:57 +0800

    修改

--- 03.Src/Calculator/Calculator.Core/Calculator_Operation/Operation.cs
+++ 03.Src/Calculator/Calculator.Core/Calculator_Operation/Operation.cs
@@ -8,33 +8,6 @@ namespace Calculator.Core
 {
     public interface IOperation
     {
-
-        private double _numberA = 0;
-        private double _numberB = 0;
-        //private double _result = 0;
-       // private string _symbol = null;
-        
-        public double NumberA
-        {
-            get { return _numberA; }
-            set { _numberA = value; }
-        }
-
-        public double NumberB
-        {
-            get { return _numberB; }
-            set { _numberB = value; }
-        }
-
-        public virtual object GetResult()
-        {
-            double result = 0;
-            return result;
-        }
-        
-
-
         double GetResult(double number1, double number2);
-        {
     }
 }
