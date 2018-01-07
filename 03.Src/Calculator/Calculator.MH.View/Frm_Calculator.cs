cfd70dc4e3145cf5f2db91f7a421d9ce35455eee
mmmhong <mh_jsnt@163.com>
2018-01-07 11:43:27 +0800

    呵呵

--- 03.Src/Calculator/Calculator.MH.View/Frm_Calculator.cs
+++ 03.Src/Calculator/Calculator.MH.View/Frm_Calculator.cs
@@ -17,7 +17,7 @@ namespace Calculator
     public partial class Frm_Calculator : Form
     {
         ScreenData screenData = new ScreenData();
-<<<<<<< HEAD
+
         Operation oper = new Operation();
         Special_Operation speOper = new Special_Operation();
 
@@ -29,9 +29,7 @@ namespace Calculator
         private bool _lastIsSymbol = false;
         private bool _lastIsSpecial = false;
         private string result = "0";
-=======
-        IOperation oper;
->>>>>>> 8ba5894f325e10dcd5dc56769c10d852fcdfe96b
+
 
         public Frm_Calculator()
         {
