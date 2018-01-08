fead809ae14ee7fcfd03bbf66ce2a861185ed842
Lizhonngxi <15961170229@163.com>
2018-01-08 14:50:45 +0800

    1

--- 03.Src/Calculator/Calculator.Core/Core/CalcException.cs
+++ 03.Src/Calculator/Calculator.Core/Core/CalcException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculator.Core.Core
+namespace Calculator.Core
 {
     public class CalcException : Exception
     {
