5e9d48bdc504bc9834863c18bdc1aa52be5942a0
Lizhonngxi <15961170229@163.com>
2018-01-19 14:48:36 +0800

    修改错误

--- 03.Src/Calculator/Calculator.Core/SDK/Calc.cs
+++ 03.Src/Calculator/Calculator.Core/SDK/Calc.cs
@@ -174,7 +174,17 @@ namespace Calculator.Core.SDK
         /// <returns></returns>
         public string GetFormula()
         {
-            return Exp.ToString();
+            string str = null;
+            try
+            {
+               str= Exp.ToString();
+            }
+            catch (Exception)
+            {
+                Exp.Locked = true;
+               
+            }
+            return str;
         }
     }
 }
